48aae12c5b16d9882ca3af2f80bdb7f109bbf05f
Shoichi Kanzaki <zaksho1121@gmail.com>
2018-02-09 16:11:12 +0900

    project init

---
+++ HandDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandDraw
+//
+//  Created by Shoichi Kanzaki on 2018/02/09.
+//  Copyright © 2018年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
